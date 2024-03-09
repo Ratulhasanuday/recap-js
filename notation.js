870f406753b28f04facbdee89558cbb9ba1347dd
Ratulhasanuday <ratulhasanudoy412220@gmail.com>
2024-03-09 10:28:11 +0600

    notation

---
+++ notation.js
@@ -0,0 +1,10 @@
+const person={
+    name:'ratul',
+    profession:'web developer',
+    age:20,
+    address:'mymensingh'
+}
+//dot notation
+const prof=person.profession;
+//bracket notation
+const prof1=person['profession']
